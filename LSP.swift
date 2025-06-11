5b62ea8d91139d3f7454b6845e6602e66a77dd86
vcosmin2701 <v.cosmin2717@gmail.com>
2025-06-11 15:32:03 +0300

    LSP - Practice section

--- LSP.swift
+++ LSP.swift
@@ -75,3 +75,99 @@ refuelCar(bmw)
 
 let tesla = ElectricCar()
 // refuelCar(tesla) -> compilation error, which is good
+
+/// MARK: Practice section
+
+/***
+class Bird {
+    func fly() {
+        print("The bird is flying high in the sky")
+    }
+
+    func eat() {
+        print("The bird is eating")
+    }
+}
+
+class Penguin: Bird {
+    override func fly() {
+        fatalError("Penguins can't fly")
+    }
+
+    func swim() {
+        print("The penguin is swimming")
+    }
+}
+
+func makeBirdFly(_ bird: Bird) {
+    print("Attempting to make a bird fly...")
+    bird.fly()
+    print("---")
+}
+
+let sparrow = Bird()
+print("Testing with a Sparrow:")
+makeBirdFly(sparrow)
+
+let penguin = Penguin()
+print("Testing with a Penguin:")
+makeBirdFly(penguin
+***/
+
+// Practice solution
+
+protocol Flyabe {
+    func fly()
+}
+
+protocol Swimable {
+    func swim()
+}
+
+class Bird {
+    func eat() {
+        print("The bird is eating")
+    }
+}
+
+class Penguin: Bird, Swimable {
+    func swim() {
+        print("Penguins can swim")
+    }
+
+    override func eat() {
+        print("Penguins eat fish")
+    }
+}
+
+class Sparrow: Bird, Flyabe {
+    func fly() {
+        print("Sparrows can fly")
+    }
+
+    override func eat() {
+        print("Sparrows eat seeds")
+    }
+}
+
+func makeBirdFly(_ bird: Flyabe) {
+    print("Attempting to make a bird fly...")
+    bird.fly()
+    print("---")
+}
+
+func makeBirdSwim(_ bird: Swimable) {
+    print("Attempting to make a bird swim...")
+    bird.swim()
+    print("---")
+}
+
+let sparrow = Sparrow()
+print("Testing with a Sparrow:")
+makeBirdFly(sparrow)
+
+let penguin = Penguin()
+print("Testing with a Penguin:")
+// makeBirdFly(penguin) // This would cause a compilation error, which is good
+makeBirdSwim(penguin)
+
